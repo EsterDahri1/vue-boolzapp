f1b53abcae85fd9cf14ffa9e61c772a684850302
EsterDahri <esterdahri@gmail.com>
2023-09-14 11:59:33 +0200

    add initial markup, links and check if all is ok

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+/*Test to se if everything is linked correctly*/
+//console.log('hello');
+
+/* 
+Milestone 1
+1 - Replica della grafica con la possibilità di avere messaggi scritti dall'utente (verdi) e dall'interlocutore (bianco) assegnando due classi CSS diverse
+2 - Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+*/
+
+/*const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')*/
