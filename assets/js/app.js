caf67bf5e74105a3c59081c49d26b6a888d99f83
EsterDahri <esterdahri@gmail.com>
2023-09-15 13:07:57 +0200

    add M4 description and link to useful technique

--- assets/js/app.js
+++ assets/js/app.js
@@ -239,4 +239,12 @@ createApp({
         }
 
     }
+    /*
+    Milestone 4
+    ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+    contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+    “mar” rimangono solo Marco e Martina)   https://stackoverflow.com/questions/69287834/search-bar-vue-js
+    */
+
+    
 }).mount('#app')
