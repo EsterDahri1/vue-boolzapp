d129b4867a22b0d684f10847f571ba13d1817015
EsterDahri <esterdahri@gmail.com>
2023-09-15 11:10:21 +0200

    add activeContact's  name and avatar in col-7

--- assets/js/app.js
+++ assets/js/app.js
@@ -188,6 +188,13 @@ createApp({
         }
     },
 
+    /*
+    Milestone 2
+    ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+    messaggi relativi al contatto attivo all’interno del pannello della conversazione
+    ● Click sul contatto mostra la conversazione del contatto cliccato
+    */
+
     methods:{
         addActiveContact (index){
             this.activeContact = index;
